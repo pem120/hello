05b4c1bb65cbcd497fb01789fb4882848169591a
pem120 <87648658+pem120@users.noreply.github.com>
2022-09-30 13:50:38 +0530

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "kerby":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+f f f f f f 3 3 3 3 f f f f f f 
+f 1 1 1 1 f 3 3 3 3 f 1 1 1 1 f 
+f 1 f f 1 f f f f f f 1 f f 1 f 
+f 1 f f 1 f f f f f f 1 f f 1 f 
+f 1 1 1 1 f 3 3 3 3 f 1 1 1 1 f 
+f f f f f f 3 3 3 3 f f f f f f 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 f 3 3 3 3 3 3 3 3 f 3 3 3 
+3 3 3 f 3 3 3 3 3 3 3 3 f 3 3 3 
+3 3 3 f 3 3 3 3 3 3 3 3 f 3 3 3 
+3 3 3 f f 3 3 3 3 3 3 f f 3 3 3 
+3 3 3 3 f f f f f f f f 3 3 f 3 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
